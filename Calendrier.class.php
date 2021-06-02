51ff3ef347156795c58f013a523d37f1820b1b7e
mickael <chalder@hotmail.fr>
2021-06-02 10:46:06 +0200

    classCalendrier et fonction ok

--- Calendrier.class.php
+++ Calendrier.class.php
@@ -18,12 +18,26 @@ class Calendrier {
     public static function buildNewCalendrier($post,$id=null) {
 
         $newCalendrier = new Datetime($post);
+        $newCalendrier->add(new DateInterval('P1M')); 
         $newCalendrier->id = $id;
-        $newCalendrier->newDate = $newCalendrier->add(new DateInterval('P1M')); $newCalendrier->format('Y-m-d');
-        $newCalendrier->newYear = $newCalendrier->format('Y-m-d');
-        $newCalendrier->newMonth =$newCalendrier->format('Y-m-d');
-        $newCalendrier->newDay = $newCalendrier->format('Y-m-d');
+        $newCalendrier->newDate = $newCalendrier->format('Y-m-d');
+        $newCalendrier->newYear = $newCalendrier->format('Y');
+        $newCalendrier->newMonth =$newCalendrier->format('m');
+        $newCalendrier->newDay = $newCalendrier->format('d');
         return $newCalendrier;
 
     }
+
+    public static function nombreJourMois($month,$year) {
+
+        $NbrDeJour=date("t",mktime(1,1,1,intval($month),1,intval($year)));
+        return $NbrDeJour;
+    }
+    
+    public static function PremierJourDuMois($month,$year) {
+
+        $premierJour=date("w",mktime(1,1,1,intval($month),1,intval($year)));
+        return $premierJour;
+    }
+
 }
